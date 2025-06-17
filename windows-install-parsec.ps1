d7809cd3760c959ca8846dbe4f53666b3e1d144a
Dec13th <149968328+December13th@users.noreply.github.com>
2025-06-17 08:39:18 -0400

    Delete windows-install-parsec.ps1

--- windows-install-parsec.ps1
+++
@@ -1,6 +0,0 @@
-$parsecUrl = "https://builds.parsecgaming.com/package/parsec-windows.exe"
-$installerPath = "C:\Windows\Temp\parsec-windows.exe"
-Invoke-WebRequest -Uri $parsecUrl -OutFile $installerPath
-Start-Process -FilePath $installerPath -ArgumentList "/S" -Wait
-Remove-Item $installerPath
-Start-Process "C:\Program Files\Parsec\parsec.exe"
